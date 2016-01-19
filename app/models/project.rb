58a36e02cb6b7518e0e8e610258e765287d31428
Carlos Gajardo <cjgajard@gmail.com>
2016-01-19 20:03:24 -0300

    basic seed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, dependent: :destroy
+
+  before_save :verify_name
+
+  def verify_name
+    if self.name.empty?
+      self.name = "Proyecto #{self.id}"
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+  {
+    name: Faker::Name.name,
+    email: 'test@mail.com',
+    password: 'password'
+  },
+  {
+    name: Faker::Name.name,
+    email: 'test2@mail.com',
+    password: 'password'
+  },
+  {
+    name: Faker::Name.name,
+    email: 'test3@mail.com',
+    password: 'password'
+  }
+])
+
+1.upto(6) do |i|
+  Project.create(user: users.sample)
+end
+
+projects = Project.all
+
+1.upto(projects.count * 5) do |i|
+  Task.create(
+    user: users.sample,
+    project: projects.sample,
+    status: rand(0..2)
+  )
+end
